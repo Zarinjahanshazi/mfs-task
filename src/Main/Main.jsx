c7db6c24ed05fefbab1cfd5d616a0414ff1a0376
Zarinjahanshazi <zarinshazi@gmail.com>
2024-08-17 19:21:17 +0600

    final

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -11,7 +11,7 @@ export const router = createBrowserRouter([
         {
 path:"/",
 element:<Home/>,
-loader:()=>fetch(`https://scic-server-kappa.vercel.app/products`)
+loader:()=>fetch(`https://mfs-server-pink.vercel.app/products`)
         }
      
       ]
